5d167c5e41090542c7112b807e046518ade50b12
구태호 <gth0908@naver.com>
2024-04-04 22:27:04 +0900

    검색창을 통해 검색한 정보의 영화만 출력

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="navbar">
+        <a href="#">Home</a>
+        <a href="#">Movies</a>
+        <a href="#">About</a>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavbarComponent", //컴포넌트 이름: 파일명 + component
+};
+</script>
+
+<style>
+.navbar {
+    background: #000;
+    padding: 20px;
+    text-align: center;
+}
+
+.navbar a {
+    color: #fff;
+    text-decoration: none;
+    padding: 1em;
+}
+</style>
